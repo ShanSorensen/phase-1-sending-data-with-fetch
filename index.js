2c0f8237ab322de5f163a448d06a0f3e9c211fcf
Shannon Sorensen <sorensen.sms@gmail.com>
2021-09-30 14:26:42 -0500

    commit tree 8f63bc32c4a0be1924f972526132e778c7101178

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+const baseURL = 'http://localhost:3000'
+const formData = { 
+    userName: "${username}",
+    email: "${email}",
+};
+
+function submitData(userName, email) {
+    fetch(baseURL + `/users/`, configObj)
+    .then
+    return fetch()
+}
+
+const configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify(formData)
+};
+fetch(baseURL + '/users/', configurationObject)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function(object) {
+        console.log(object);
+    });
